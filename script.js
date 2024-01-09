ecdc5c129bb30e59baf345b42a2b2d6a4d5f8afc
darshan1005 <battuladarshan7838@gmail.com>
2024-01-09 12:08:09 +0530

    msg: curr-converter

--- script.js
+++ script.js
@@ -12,73 +12,70 @@ let resultBox = document.querySelector('.container .result-box');
 let result = document.querySelector('.container .result-box .result');
 let switchBtn = document.querySelector('.container .switch-btn');
 
+//Symbols Api
 let symbolsApiUrl = 'https://api.exchangerate.host/symbols';
 let apiKey = 'eb285e81c7865fde916a5361';
-let currFromValue, currFromFlagSrc, currToFlagSrc, currToValue;
+let currFromValue, currFromFlagSrc, currToValue, currToFlagSrc;
 
-fromInputFlag.addEventListener('click', ()=>{
-    fromOptionsBox.classList.toggle('active');
+fromInputBox.addEventListener('click', () => {
+    fromOptionsBox.classList.toggle('active')
     toOptionsBox.classList.remove('active');
-});
+})
 
-toOptionsBox.addEventListener('click', ()=>{
-    fromOptionsBox.classList.remove('active');
+toInputBox.addEventListener('click', () => {
     toOptionsBox.classList.toggle('active');
+    fromOptionsBox.classList.remove('active');
 });
 
-let getCountrys = () =>{
+let getCountry = () => {
     let fromLi = '';
     let toLi = '';
     for (currency_code in country_list) {
         fromLi += `<li onclick="getFromValue('${currency_code}')"><img src="https://flagsapi.com/${country_list[currency_code]}/flat/64.png" alt="">${currency_code}</li>`;
         toLi += `<li onclick="getToValue('${currency_code}')"><img src="https://flagsapi.com/${country_list[currency_code]}/flat/64.png" alt="">${currency_code}</li>`;
     }
-
     fromOptionsBox.innerHTML = fromLi;
     toOptionsBox.innerHTML = toLi;
 }
 
-let getFromValue = (country) =>{
+let getFromValue = (country) => {
     fromInputFlag.src = `https://flagsapi.com/${country_list[country]}/flat/64.png`;
     fromInput.value = country;
-    fromOptionsBox.classList.remove ('active');
+    fromOptionsBox.classList.remove('active');
 }
 
-let getToValue = (country) =>{
+let getToValue = (country) => {
     toInputFlag.src = `https://flagsapi.com/${country_list[country]}/flat/64.png`;
     toInput.value = country;
     toOptionsBox.classList.remove('active');
 }
 
 let getExchangeRate = () => {
-    if (amount.value !== ''){
-        result.innerHTML = 'Get Exchange rate....';
+    if (amount.value != '') {
+        result.innerHTML = 'Getting Exchange Rate....';
         result.style.fontSize = '12px';
         let url = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/${fromInput.value}`;
-        fetch(url).then(res => res.json()).then(data=>{
+        fetch(url).then(res => res.json()).then(data => {
             let exchangeRate = data.conversion_rates[toInput.value];
-            let finalExchangeRate = (amount.value * exchangeRate).toFixed(3);
+            let finalExchangeRate = (amount.value * exchangeRate).toFixed(2);
             result.innerHTML = `${amount.value} ${fromInput.value} = ${finalExchangeRate} ${toInput.value}`;
-            result.style.display = '15px';
+            result.style.fontSize = '15px'
         })
-        resultBox.style.display = 'block'
+        resultBox.style.display = 'block';
     }
 }
 
-switchBtn.addEventListener('click', ()=>{
-    // assign
+switchBtn.addEventListener('click', () => {
     currFromValue = fromInput.value;
     currToValue = toInput.value;
-    currFromFlagSrc = fromInputFlag.value;
-    currToFlagSrc = toInputFlag.value;
-    // exchange
+    currFromFlagSrc = fromInputFlag.src;
+    currToFlagSrc = toInputFlag.src;
     fromInput.value = currToValue;
     toInput.value = currFromValue;
     fromInputFlag.src = currToFlagSrc;
     toInputFlag.src = currFromFlagSrc;
-
     getExchangeRate();
 });
 
-getCountrys();
-convertBtn.addEventListener ('click', getExchangeRate);
+getCountry();
+convertBtn.addEventListener('click', getExchangeRate);
